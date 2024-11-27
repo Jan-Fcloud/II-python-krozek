f3031251af75e3587fec7e4b220e832a7bf6903a
Jan-Fcloud <jan@jansbyte.com>
2024-11-27 15:31:21 +0100

    naloga fix

--- Naloge/02 - Oseba.py
+++ Naloge/02 - Oseba.py
@@ -9,7 +9,7 @@ Naredite razred "Oseba", ki vsebuje naslednje vrednosti:
 
 2. V razredu napišite funkcijo, ki kot parameter dobi text, ki predstavlja ime prijatelja, ter se doda v list prijateljev razreda (za vnos je input()).
 
-3. V glavnem programu (izven razreda) naredite while zanko, ki nenehno kliče funkcijo za vpis prijatelja in neha samo če bo vaš vpis STOP.
+3. V glavnem programu ustvarite osebo z pomočjo vnosa (input()) in jo na koncu izpišite
 
 4. Razredu dodajte funkcijo, ki izpiše "<ime> je star/a več kot 21" ali pa, da ni. To se zve z atributom starost vašega razreda.
 
