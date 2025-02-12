d1611971919ee0a36d41dc11e478a6fff578bf5b
Jan-Fcloud <jan@jans.dev>
2025-02-12 12:52:00 +0000

    import update

--- Primeri/Uvazanje python datotek/glavno.py
+++ Primeri/Uvazanje python datotek/glavno.py
@@ -1,5 +1,8 @@
 """
 V tem primeru bomo naredili program, ki bo uvozil našo drugo python datoteko, ter klical njeno glavno funkcijo.
+
+Uvazanje nam omogoča, da izvedemo kodo, ki je v drugi datoteki, tudi če je ta datoteka uvozila našo glavno datoteko.
+Zelo uporabno je pri večjih programih, ki jih želimo razdeliti na več datotek za lažjo preglednost in razumevanje.
 """
 
 # Uvozimo našo drugo python datoteko
