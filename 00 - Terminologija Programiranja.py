e0485dabdcb841f8e0ab4b0366f07c7457055f84
Jan-Fcloud <jan@jans.dev>
2025-02-12 12:50:43 +0000

    terminologija

--- 00 - Terminologija Programiranja.py
+++ 00 - Terminologija Programiranja.py
@@ -1,1 +1,62 @@
-# TODO
+"""
+V tem primeru bomo si ogledali nekatere osnovne terminologije programiranja.
+
+Podatkovni tipi:
+- int: cela števila
+- float: decimalna števila
+- str: nizovi znakov
+- bool: logične vrednosti (True/False)
+
+Inicializacija:
+- Ustvarjanje spremenljivk
+
+Vnos podatkov:
+- input: bere vrednosti
+- print: izpisuje vrednosti
+
+Zanka:
+- for: za vsak element v seznamu
+- while: dokler je izpolnjen pogoj
+
+Pogoj:
+- if: če je izpolnjen pogoj
+- else: če ni izpolnjen pogoj
+
+Funkcija:
+- def: definiramo funkcijo
+- return: vrne vrednost
+
+Parametri:
+- argumenti funkcije (to kar je znotraj oklepajev funkcije)
+
+Skupki podatkov:
+- seznami (list) - vsebina je lahko karkoli
+- slovarji (dict) - ključi in vrednosti
+- nizi (str) - list znakov ("Zdravo" = 'Z', 'd', 'r', 'a', 'v', 'o')
+
+Casting - pretvarjanje tipov:
+- int() - pretvori v celo število
+- float() - pretvori v decimalno število
+- str() - pretvori v niz
+
+Error - napaka:
+- NameError - ime spremenljivke ni pravilno
+- TypeError - tip podatka ni pravilen
+- ValueError - vrednost podatka ni pravilna
+- IndexError - index je izven meja seznama
+- KeyError - ključ ni v slovarju
+
+Error handling - obdelava napak:
+- try: poskusimo izvesti kodo
+- except: če pride do napake, izvedi ta kod
+- finally: če ni napak, izvedi ta kod
+
+Vrsta napak:
+- Syntax Error - sintaksa je napačna (npr. nerazumljiv zapis)
+- Semantic Error - pomen je napačen (npr. nepravilno ime spremenljivke)
+- Logical Error - logična napaka (npr. nepravilno logično izračunavanje)
+- Runtime Error - napaka, ki se pojavi med izvajanjem programa (npr. deljenje z 0)
+
+"""
+
+
